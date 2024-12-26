96b757c547fcd5a078a919126bc30ca6e6aedbb1
Kaira Diagne <diagnekaira@yahoo.com>
2024-12-26 23:31:30 +0100

    Format using Prettier.

--- src/exercises/exercise-response.dto.ts
+++ src/exercises/exercise-response.dto.ts
@@ -1,42 +1,42 @@
 enum ExerciseDifficulty {
-    Beginner = "Beginner",
-    Intermediate = "Intermediate",
-    Advanced = "Advanced",
+  Beginner = 'Beginner',
+  Intermediate = 'Intermediate',
+  Advanced = 'Advanced',
 }
 
 enum ExerciseForceType {
-    Push = "Push",
-    Pull = "Pull",
-    Plyometric = "Plyometric",
+  Push = 'Push',
+  Pull = 'Pull',
+  Plyometric = 'Plyometric',
 }
 
 enum ExerciseMechanic {
-    Compound = "Compound",
-    Isolation = "Isolation"
+  Compound = 'Compound',
+  Isolation = 'Isolation',
 }
 
 enum ExerciseEquipment {
-    Barbell = "Barbell",
-    Bodyweight = "Bodyweight",
-    Box = "Box",
-    Dumbbell = "Dumbbell",
-    Machine = "Machine",
-    Sled = "Sled",
-    Other = "Other",
+  Barbell = 'Barbell',
+  Bodyweight = 'Bodyweight',
+  Box = 'Box',
+  Dumbbell = 'Dumbbell',
+  Machine = 'Machine',
+  Sled = 'Sled',
+  Other = 'Other',
 }
 
 export class ExerciseResponseDTO {
-    id: string;
-    name: string;
-    difficulty: ExerciseDifficulty;
-    forceType: ExerciseForceType;
-    mechanic: ExerciseMechanic;
-    equipment: ExerciseEquipment;
-    primaryMuscleGroups: MuscleGroupResponseDTO[];
-    secondaryMuscleGroups: MuscleGroupResponseDTO[];
+  id: string;
+  name: string;
+  difficulty: ExerciseDifficulty;
+  forceType: ExerciseForceType;
+  mechanic: ExerciseMechanic;
+  equipment: ExerciseEquipment;
+  primaryMuscleGroups: MuscleGroupResponseDTO[];
+  secondaryMuscleGroups: MuscleGroupResponseDTO[];
 }
 
 export class MuscleGroupResponseDTO {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
--- src/exercises/exercises.service.ts
+++ src/exercises/exercises.service.ts
@@ -6,17 +6,20 @@ import { ExerciseResponseDTO } from './exercise-response.dto';
 
 @Injectable()
 export class ExercisesService {
-    constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {}
 
-    async getExercises(): Promise<ExerciseResponseDTO[]> {
-        const filePath = this.configService.get("EXERCISE_LIST_PATH");
-        const jsonString = await this.loadFileAsync(filePath);
-        const json = JSON.parse(jsonString);
-        const exercises: ExerciseResponseDTO[] = plainToInstance(ExerciseResponseDTO, json as object[]);
-        return exercises;
-    }
+  async getExercises(): Promise<ExerciseResponseDTO[]> {
+    const filePath = this.configService.get('EXERCISE_LIST_PATH');
+    const jsonString = await this.loadFileAsync(filePath);
+    const json = JSON.parse(jsonString);
+    const exercises: ExerciseResponseDTO[] = plainToInstance(
+      ExerciseResponseDTO,
+      json as object[],
+    );
+    return exercises;
+  }
 
-    private async loadFileAsync(filePath: string): Promise<string> {
-        return await readFile(filePath, "utf-8");
-    }
+  private async loadFileAsync(filePath: string): Promise<string> {
+    return await readFile(filePath, 'utf-8');
+  }
 }
