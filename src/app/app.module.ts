90f7717a8c87ad25f926a7e9c8fbd6bd703da0d8
Kaira Diagne <diagnekaira@yahoo.com>
2024-12-26 17:42:46 +0100

    Setup config module and cleanup app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ExercisesModule } from './exercises/exercises.module';
+import { ExercisesModule } from '../exercises/exercises.module';
 
 @Module({
   imports: [ExercisesModule],
