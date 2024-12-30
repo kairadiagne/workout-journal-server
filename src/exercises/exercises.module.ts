310849994ed959373804e1b646f2dcf1823b5d72
Kaira Diagne <diagnekaira@yahoo.com>
2024-12-30 20:44:53 +0100

    Directly load json data so that we don't have to bundle the file with the app.

--- src/exercises/exercises.module.ts
+++ src/exercises/exercises.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ExercisesService } from './exercises.service';
 import { ExercisesController } from './exercises.controller';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule],
   controllers: [ExercisesController],
   providers: [ExercisesService],
 })
--- src/exercises/exercises.service.ts
+++ src/exercises/exercises.service.ts
@@ -1,25 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { readFile } from 'fs/promises';
-import { ConfigService } from '@nestjs/config';
 import { plainToInstance } from 'class-transformer';
 import { ExerciseResponseDTO } from './exercise-response.dto';
+import exercisesData from './exercises_v1.json';
 
 @Injectable()
 export class ExercisesService {
-  constructor(private readonly configService: ConfigService) {}
-
   async getExercises(): Promise<ExerciseResponseDTO[]> {
-    const filePath = this.configService.get('EXERCISE_LIST_PATH');
-    const jsonString = await this.loadFileAsync(filePath);
-    const json = JSON.parse(jsonString);
     const exercises: ExerciseResponseDTO[] = plainToInstance(
       ExerciseResponseDTO,
-      json as object[],
+      exercisesData,
     );
     return exercises;
-  }
-
-  private async loadFileAsync(filePath: string): Promise<string> {
-    return await readFile(filePath, 'utf-8');
   }
 }
